8a6151c86288e8f82f8c6fb6bedd67ee284b218c
Rogisimo <robertasmurnikovas1@gmail.com>
2022-08-27 14:26:44 +0300

    Layout change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ renderQoutes()
 function quoteHTML(quote){
     return `<div class="quote-card">
     <h2 class="quote-card-anime-name">${quote.anime}</h2>
-    <h2 class="qoute-card-anime-character-name">${quote.character}</h2>
+    <h2 class="qoute-card-anime-character-name">~${quote.character}</h2>
     <h4 class="qoute-card-qoute">${quote.quote}</h4>
 </div>`
 }
