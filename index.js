b4ef3479a9907d3dd7643b60342c5bd99a8fb2b6
Rogisimo <robertasmurnikovas1@gmail.com>
2022-08-27 22:08:55 +0300

    quote card css

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 // API https://animechan.vercel.app/api/quotes
 
 
-async function renderQoutes(){
+async function renderQuotes(){
     const quotes = await fetch("https://animechan.vercel.app/api/quotes")
     const quotesData = await quotes.json("")
     const quotesListEl = document.querySelector(' .quote-cards')
     quotesListEl.innerHTML = quotesData.map((quote) => quoteHTML(quote)).join("")
 }
 
-renderQoutes()
+//renderQuotes()
 
 function quoteHTML(quote){
-    return `<div class="quote-card">
-    <h2 class="quote-card-anime-name">${quote.anime}</h2>
-    <h2 class="qoute-card-anime-character-name">~${quote.character}</h2>
-    <h4 class="qoute-card-qoute">${quote.quote}</h4>
-</div>`
+    return ``
 }
