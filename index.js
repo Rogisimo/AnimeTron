1be2de2cfbcd0f40f67dc5c43bb37bc6eb456938
Rogisimo <robertasmurnikovas1@gmail.com>
2022-08-28 17:26:41 +0300

    Search by anime name

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-// API https://animechan.vercel.app/api/quotes
+// API https://animechan.vercel.app/api/quotes 10 random
+//https://animechan.vercel.app/api/quotes/anime?title=naruto by anime name
+//https://animechan.vercel.app/api/quotes/character?name=saitama by character name
 
+
+
+async function searchByAnimeName(){
+    let input = document.getElementById("searchInput").value;
+    const quotes = await fetch(`https://animechan.vercel.app/api/quotes/anime?title=${input}`)
+    const quotesData = await quotes.json("")
+    const quotesListEl = document.querySelector(' .quote-cards')
+    quotesListEl.innerHTML = quotesData.map((quote) => quoteHTML(quote)).join("")
+    //document.getElementById("quotes-header").innerHTML = `Search results for: ${input}`
+}
 
 async function renderQuotes(){
     const quotes = await fetch("https://animechan.vercel.app/api/quotes")
@@ -13,7 +25,8 @@ renderQuotes()
 function quoteHTML(quote){
     return `<div class="quote-card">
     <h2 class="quote-card-anime-name">${quote.anime}</h2>
-    <p class="qoute-card-qoute">${quote.quote} Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas earum autem corporis odit nemo recusandae dicta, ipsum cum maiores consequatur obcaecati quasi ea vero nisi expedita quam facilis fuga soluta reprehenderit? Modi odit numquam saepe et quas eum quisquam?</p>
+    <p class="qoute-card-qoute">${quote.quote}</p>
     <p class="qoute-card-anime-character-name">~${quote.character}</p>
 </div>`
 }
+
