5458ce21d099a709ce46f51ff5b08c7b62fea9fd
Rogisimo <robertasmurnikovas1@gmail.com>
2022-08-28 16:40:37 +0300

    CSS update

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ async function renderQuotes(){
     quotesListEl.innerHTML = quotesData.map((quote) => quoteHTML(quote)).join("")
 }
 
-//renderQuotes()
+renderQuotes()
 
 function quoteHTML(quote){
-    return ``
+    return `<div class="quote-card">
+    <h2 class="quote-card-anime-name">${quote.anime}</h2>
+    <p class="qoute-card-qoute">${quote.quote} Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas earum autem corporis odit nemo recusandae dicta, ipsum cum maiores consequatur obcaecati quasi ea vero nisi expedita quam facilis fuga soluta reprehenderit? Modi odit numquam saepe et quas eum quisquam?</p>
+    <p class="qoute-card-anime-character-name">~${quote.character}</p>
+</div>`
 }
