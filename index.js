65d1d6daa18ed6c04967d132b33e1680a823ea15
Rogisimo <robertasmurnikovas1@gmail.com>
2022-08-30 19:20:13 +0300

    loading state

--- index.js
+++ index.js
@@ -2,25 +2,41 @@
 //https://animechan.vercel.app/api/quotes/anime?title=naruto by anime name
 //https://animechan.vercel.app/api/quotes/character?name=saitama by character name
 
-
+let quotes;
 
 async function searchByAnimeName(){
     let input = document.getElementById("searchInput").value;
+    document.getElementById("quotes-header").innerHTML = `Search results for: ${input}`
     const quotes = await fetch(`https://animechan.vercel.app/api/quotes/anime?title=${input}`)
     const quotesData = await quotes.json("")
     const quotesListEl = document.querySelector(' .quote-cards')
     quotesListEl.innerHTML = quotesData.map((quote) => quoteHTML(quote)).join("")
-    //document.getElementById("quotes-header").innerHTML = `Search results for: ${input}`
 }
+
+async function loading(){
+    const quotesWrapper = document.querySelector('.quotes');
+    quotesWrapper.classList += ' quotes-loading'
+    if(!quotes){
+        quotes = await renderQuotes();
+      }
+    
+      quotesWrapper.classList.remove('books__loading')
+}
+
 
 async function renderQuotes(){
-    const quotes = await fetch("https://animechan.vercel.app/api/quotes")
+    quotes = await fetch("https://animechan.vercel.app/api/quotes")
     const quotesData = await quotes.json("")
     const quotesListEl = document.querySelector(' .quote-cards')
-    quotesListEl.innerHTML = quotesData.map((quote) => quoteHTML(quote)).join("")
+    quotesListEl.innerHTML = quotesData.map((quote) => quoteHTML(quote)).join("");
 }
 
-renderQuotes()
+
+setTimeout(() => {
+    loading()
+  });
+
+
 
 function quoteHTML(quote){
     return `<div class="quote-card">
